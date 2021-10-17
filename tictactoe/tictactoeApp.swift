501349969348f009e11c81f76237352e490e13ef
klpindoria <44475172+klpindoria@users.noreply.github.com>
2021-10-17 20:15:43 +0100

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Ketan Pindoria on 17/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
