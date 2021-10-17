98d5ac6f45c2291f08269eca7571329edc8e5d93
klpindoria <44475172+klpindoria@users.noreply.github.com>
2021-10-17 23:00:10 +0100

    Game now complete. Computer now plays very well against a Human. Code is not structured well. Time to clean it up now.

---
+++ tictactoe/Alerts.swift
@@ -0,0 +1,29 @@
+//
+//  Alerts.swift
+//  tictactoe
+//
+//  Created by Ketan Pindoria on 17/10/2021.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    var buttonTitle: Text
+}
+
+struct AlertContext {
+    static let humanWin = AlertItem(title: Text("You Win!"),
+                                    message: Text("Well done you have won against the computer"),
+                                    buttonTitle: Text("Awesome Work!"))
+    
+    static let computerWin = AlertItem(title: Text("You Lost!"),
+                                       message: Text("Better luck next time you lost against the computer"),
+                                       buttonTitle: Text("Try Again!"))
+    
+    static let draw = AlertItem(title: Text("It's a Draw!"),
+                                message: Text("Seems you have tied with the computer"),
+                                buttonTitle: Text("Almost there!"))
+}
--- tictactoe/ContentView.swift
+++ tictactoe/ContentView.swift
@@ -15,9 +15,8 @@ struct ContentView: View {
     
     // Create moves array filled with 9 nils - for tracking player selection
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
-    
-    //Temp State for testing purposes
-    @State private var isHumanTurn = true
+    @State private var isBoardDisabled = false
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         GeometryReader { geometry in
@@ -39,15 +38,53 @@ struct ContentView: View {
                         }
                         // Set X or O on player click & toggle between human & computer
                         .onTapGesture {
+                            // Register human tap and assign to Move[] array
                             if isCircleOccupied(in: moves, forIndex: i) { return }
-                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
-                            isHumanTurn.toggle()
+                            moves[i] = Move(player: .human, boardIndex: i)
+                                                                                   
+                            //checking for win or draw scenario
+                            if checkWinScenario(for: .human, in: moves) {
+                                alertItem = AlertContext.humanWin
+                                return
+                            }
+                            
+                            if checkDrawScenario(in: moves) {
+                                alertItem = AlertContext.draw
+                                return
+                            }
+                            
+                            //prevent human from clicking another circle before computers move
+                            isBoardDisabled = true
+                            
+                            // Auto-play computer move and assign to Move[] array
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+                                let computerPosition = determineComputerMovePosition(in: moves)
+                                moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
+                                isBoardDisabled = false
+                                
+                                //checking for win or draw scenario
+                                if checkWinScenario(for: .computer, in: moves) {
+                                    alertItem = AlertContext.computerWin
+                                    return
+                                }
+                                
+                                if checkDrawScenario(in: moves) {
+                                    alertItem = AlertContext.draw
+                                    return
+                                }
+                            }
                         }
                     }
                 }
                 Spacer()
             }
             .padding()
+            .disabled(isBoardDisabled)
+            .alert(item: $alertItem, content: { alertItem in
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
+                      dismissButton: .default(alertItem.buttonTitle, action: { resetGame() }))
+            })
         }
     }
     
@@ -56,15 +93,77 @@ struct ContentView: View {
         return moves.contains(where: { $0?.boardIndex == index})
     }
     
+    //If Comp can win, take the win
+    //If Comp can't win, then block
+    //If Comp can't block, then take the middle
+    //If Comp can't take middle, take any random spot
     func determineComputerMovePosition(in moves: [Move?]) -> Int {
+        //If Comp can win, take the win
+        let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        //Check computers current moves taken
+        let computerMoves = moves.compactMap { $0 }.filter { $0.player == .computer }
+        let computerPosition = Set(computerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(computerPosition)
+            
+            if winPositions.count == 1 {
+                let isSpotAvailable = !isCircleOccupied(in: moves, forIndex: winPositions.first!)
+                if isSpotAvailable { return winPositions.first! }
+            }
+        }
+        
+        //If Comp can't win, then block
+        let humanMoves = moves.compactMap { $0 }.filter { $0.player == .human }
+        let humanPosition = Set(humanMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(humanPosition)
+            
+            if winPositions.count == 1 {
+                let isSpotAvailable = !isCircleOccupied(in: moves, forIndex: winPositions.first!)
+                if isSpotAvailable { return winPositions.first! }
+            }
+        }
+        
+        //If Comp can't block, then take the middle
+        let centerCircle = 4
+        if !isCircleOccupied(in: moves, forIndex: centerCircle) {
+            return centerCircle
+        }
+        
         // Easy computer mode - set random position
         var movePosition = Int.random(in: 0..<9)
+        
         // Keep setting another random position if already occupied
         while isCircleOccupied(in: moves, forIndex: movePosition) {
             movePosition = Int.random(in: 0..<9)
         }
         
         return movePosition
+    }
+    
+    func checkWinScenario(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        
+        // if player has following win pattern then they have won
+        // find position of moves for player which we pass in parameter
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        
+        //filter set to show only boardIndex for above player
+        let playerPosition = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPosition){ return true }
+        
+        return false
+    }
+    
+    func checkDrawScenario(in moves: [Move?]) -> Bool {
+        return moves.compactMap { $0 }.count == 9
+    }
+    
+    func resetGame() {
+        moves = Array(repeating: nil, count: 9)
     }
 }
 
